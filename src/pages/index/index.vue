a40016f269c6ccab69d30cc878b3564ccd22ffa8
zhangziyu <1772127579@qq.com>
2023-09-15 22:43:29 +0800

    feat: 首页热门推荐

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import {getHomeBannerAPI, getHomeCategoryAPI} from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem, CategoryItem } from '@/types/home'
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
 import CategoryPanel from '@/pages/index/components/CategoryPanel.vue'
+import HotPanel from '@/pages/index/components/HotPanel.vue'
 
 // 轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -31,6 +32,7 @@ onLoad(() => {
   <CustomNavbar />
   <CommonSwiper :list="bannerList" />
   <CategoryPanel :list="categoryList" />
+  <HotPanel />
   <view class="index">index</view>
 </template>
 
