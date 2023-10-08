1e3ffdc9b4ac179138f46fe5f046ab8dac66c463
zhangziyu <1772127579@qq.com>
2023-10-08 10:23:45 +0800

    feat: 地址管理接口及类型封装

--- src/services/address.ts
+++ src/services/address.ts
@@ -1,10 +1,18 @@
 import { request } from '@/utils/request'
-import type { AddressParams } from '@/types/address'
+import type { AddressParams, AddressItem } from '@/types/address'
 
 export const postMemberAddressAPI = (data: AddressParams) => {
   return request({
     url: '/member/address',
     method: 'POST',
     data,
+  })
+}
+
+// 获取收货地址列表
+export const getMemberAddressAPI = () => {
+  return request<AddressItem[]>({
+    url: '/member/address',
+    method: 'GET',
   })
 }
